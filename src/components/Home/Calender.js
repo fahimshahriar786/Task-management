ca4042518dc6c9522119a96b8d85696399153d84
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-07-02 09:28:35 +0600

    clander section

--- src/components/Home/Calender.js
+++ src/components/Home/Calender.js
@@ -4,7 +4,12 @@ import 'react-day-picker/dist/style.css';
 
 const Calender = () => {
   return (
-   
+    <div className='font-mono bg-lime-200 flex justify-center py-5 '>
+      <div>
+      <h1 className='text-4xl text-rose-900 font-bold mt-9'>Calender</h1>
+      <p className='mt-10 '><DayPicker /></p>
+      </div>
+    </div>
   )
 }
 
