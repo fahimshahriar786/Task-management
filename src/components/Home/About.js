7c4cb1af9dad1c3cc468067d6669c09c7ae61304
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-07-02 09:27:49 +0600

    about

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -4,7 +4,28 @@ const About = () => {
   return (
     <div className='flex justify-center bg-lime-200 text-black  '>
 
+<div class="hero min-h-screen ">
+  <div class="hero-content flex-col lg:flex-row">
+    <img src="https://img.freepik.com/free-vector/project-management-business-process-planning-workflow-organization-colleagues-working-together-teamwork_335657-2469.jpg?t=st=1656706453~exp=1656707053~hmac=d23a9bf73bbfac42c831ccee9ff765542349cc03dba4957062a972f24e75e4ac&w=1060 " class="max-w-md rounded-lg shadow-2xl" alt='about task management'/>
+    <div>
+      <h1 class="text-3xl font-bold">What is Task Management Software?</h1>
+      <p class="py-6">Task management software is used to manage your tasks, help with the estimation and scheduling, track dependencies, resources and milestones and help you make decisions when changes in priority are needed. <br/>
+
+From managing simple to-do lists to helping teams work and collaborate better, there are many different types of task management software and it is important to understand what your needs are to make sure you truly get a tool that is going to improve your productivity rather than create more work that it saves!</p>
+
+<h1 class="text-3xl font-bold">Why is Task Management Software Important?</h1>
+<p class="py-6">Traditionally, Software Development organizations use the following tools to manage their project schedules and tasks:
+
+Requirements stored in MS-Word documents, MS-Excel spreadsheets or standalone requirements capture tools
+High-level project plan (GANTT chart) developed in tools such as Microsoft Project, Primavera and printed out for reference
+Project estimates prepared using a combination of the high-level project plan and specialized standalone MS-Excel spreadsheets
+Detailed task schedules maintained by individual team members using MS-Excel, Whiteboards or Groupware solutions
+MS-Access, MS-Excel or standalone web-based bug-tracking system for tracking issues and defects associated with the project tasks.</p>
+     
+    </div>
    
+  </div>
+</div>
     
 
 
